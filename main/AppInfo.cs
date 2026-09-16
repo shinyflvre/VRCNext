d6d154b9a02b21b8f7130cfe784125139a65af83
shinyflvre <me@shinyflvres.com>
2026-09-16 18:11:42 +0200

    bump: ver

--- main/AppInfo.cs
+++ main/AppInfo.cs
@@ -2,7 +2,7 @@
 
 public static class AppInfo
 {
-    public const string Version = "2026.50.1";
+    public const string Version = "2026.60.0";
     public const string ContactEmail = "vrcn@shinyflvres.com";
     public const string Website = "vrcn.shinyflvres.com";
     public const string UserAgent = $"VRCNext/{Version} ({ContactEmail})";
